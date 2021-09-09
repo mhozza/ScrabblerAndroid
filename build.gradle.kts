065807a1f73e529278a3dc87c2ac19505a83bfd2
mhozza <mhozza@gmail.com>
2021-09-09 21:28:12 +0200

    Convert project's  build.gradle to kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext {
-        compose_version = '1.0.2'
-        kotlin_version = "1.5.21"
+    val kotlinVersion = "1.5.21"
+    extra.apply {
+        set("compose_version", "1.0.2")
+        set("kotlin_version", kotlinVersion)
     }
     repositories {
         google()
@@ -10,8 +11,8 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.0.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -22,11 +23,11 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
-        maven { url "https://kotlin.bintray.com/kotlinx" }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://kotlin.bintray.com/kotlinx") }
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.create<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
